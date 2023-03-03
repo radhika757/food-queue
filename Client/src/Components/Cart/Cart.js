125f36a88fe778b45bca05937bb9d83dce068749
[radhika] <[raoradhika2000@gmail.com]>
2023-03-03 17:38:18 +0530

    Cart updated

---
+++ Client/src/Components/Cart/Cart.js
@@ -0,0 +1,90 @@
+import React from "react";
+import styles from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import { useContext, useState } from "react";
+import CartContext from "../../Store/cart-context";
+import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+import empty from "../../assets/emptyC.png";
+
+const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+  // console.log(cartCtx);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`; // to keep 2 decimal places fixed.
+
+  // to check if the cart has items
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  // Trigger the CartProvider add ()
+  const addItemToCartHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  // order handler
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id} // we have to pass a key since its a list
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)} // onRemove prop, name same as CartItem component provider.
+          onAdd={addItemToCartHandler.bind(null, item)}
+        />
+        // <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  const modalActions = (
+    <div className={styles.actions}>
+      <button className={styles["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+
+      {/* Order btn should only show up when there are items in cart. */}
+      {hasItems && (
+        <button className={styles.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  return (
+    // key={props.id} each child in a list should have a unique key.
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {!hasItems && (
+        <div className={styles.cartpstn}>
+          <div>
+            <img src={empty} />
+          </div>
+          <div>
+            <p className={styles.empty}>Your cart feels light!</p>
+          </div>
+        </div>
+      )}
+      {!isCheckout && modalActions}
+      {/* Ex of props drilling  */}
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+    </Modal>
+  );
+};
+
+export default Cart;
